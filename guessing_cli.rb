665f9aa5557517d0d592f750fc3d064bbca4352a
chaplonglau <chaplonglau@gmail.com>
2015-06-07 22:20:19 -0700

    kool

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,25 @@
 # Code your solution here!
+
+
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  
+  num=1+rand(6)
+  num=num.to_s
+
+ input=gets.to_s.chomp
+
+  if input==num
+    puts "You guessed the correct number!"
+     
+  elsif input=="exit"
+    puts "Goodbye!"
+
+    
+  else 
+    puts "The computer guessed #{num}."
+
+  end
+  
+
+end
